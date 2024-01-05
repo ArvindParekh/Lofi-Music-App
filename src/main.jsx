97c1dd4c8217127f874f4ba95bc1c3ea3f9aa5a6
Arvind <aruparekh@gmail.com>
2024-01-05 22:28:36 +0530

    fix(sharp): address security concerns

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import sharp from 'sharp/lib/sharp.js'
+
+sharp.block({ operation: ["VipsForeignLoadWebp"] });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
