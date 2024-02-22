96bc7bb6fee79f3dc92b935e2d58f7195b3eb5dd
Arvind <aruparekh@gmail.com>
2024-02-22 18:18:41 +0530

    feat: add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {inject} from "@vercel/analytics"
 // import sharp from 'sharp'
 
 // sharp.block({ operation: ["VipsForeignLoadWebp"] });
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
