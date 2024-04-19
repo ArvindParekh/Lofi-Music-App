460e4a855230a786c91fc87c70c08ef431c6cc1d
Arvind <aruparekh@gmail.com>
2024-04-19 12:34:10 +0530

    fix: component shift due to slider

--- src/app/Footer.jsx
+++ src/app/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="border mt-7 max-w-md min-w-max mx-auto w-fit p-3 rounded-full bg-blue-50 bg-opacity-10">
+    <footer className="border mt-1 max-w-md min-w-max mx-auto w-fit p-3 rounded-full bg-blue-50 bg-opacity-10">
       Made by{" "}
       <a href="https://x.com/ArvindParekh_21" target="_blank" rel="noreferrer">
         @ArvindParekh 💓
