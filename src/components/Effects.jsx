a7f76975b4a9404af1d36af2789f791b0fb0e472
you <you@example.com>
2024-05-12 18:40:03 +0200

    New style for axis on Effects.jsx and Tracks.jsx

--- src/components/Effects.jsx
+++ src/components/Effects.jsx
@@ -3,6 +3,8 @@
 import MusicCard from "@/components/EffectCard";
 import effectsData from "../data/effects";
 
+import "@/Sliders.css"
+
 /**
  * Renders the Effects component.
  * @returns {JSX.Element} The rendered Effects component.
@@ -13,7 +15,7 @@ const Effects = () => {
       <h1 className="text-gray-400 font-medium text-xl bg-slate-800 bg-opacity-50 rounded-full inline-block px-5 py-2 my-5">
         Effects
       </h1>
-      <div className="flex items-center gap-5 lg:justify-between overflow-x-auto p-1">
+      <div className="flex items-center gap-5 lg:justify-between scroller p-1">
         {effectsData.map((effect, index) => {
           return (
             <MusicCard
