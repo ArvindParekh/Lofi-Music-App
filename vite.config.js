5955df6a0e78e100061584cdac20c8c602c624d7
Ryan Kuruppu <ryanjk.kuruppu@gmail.com>
2024-06-02 14:12:27 +0530

    Added biome

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import {resolve } from "path"
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
-  }
-})
-
+	plugins: [react()],
+	resolve: {
+		alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
+	},
+});
