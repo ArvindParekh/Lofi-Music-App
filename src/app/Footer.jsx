10a592fb8d70addb1d4d93c44c591c0b67ea321c
Arvind <aruparekh@gmail.com>
2024-07-04 23:32:46 +0530

    chore: add announcement for a feature

--- src/app/Footer.jsx
+++ src/app/Footer.jsx
@@ -1,10 +1,11 @@
 const Footer = () => {
   return (
-    <footer className="border mt-1 max-w-md min-w-max mx-auto w-fit p-3 rounded-full bg-blue-50 bg-opacity-10">
+    <footer className="border text-center mt-1 max-w-md min-w-max mx-auto w-fit p-3 rounded-full bg-blue-50 bg-opacity-10">
       Made by{" "}
       <a href="https://x.com/ArvindParekh_21" target="_blank" rel="noreferrer">
         @ArvindParekh 💓
       </a>
+      <p className="text-sm font-medium text-gray-400">Shipping a new feature soon, stay tuned!</p>
     </footer>
   );
 };
